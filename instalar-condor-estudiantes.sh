c4ebc6f24c5ebabfe912923d0b0040f408126e65
andresvia <andres.via@gmail.com>
2016-06-08 16:01:33 -0500

    configuracion de seguridad de mariadb, selinux permissive set, disco de instancia que usa free tier y cambiando a t2.micro

--- instalar-condor-estudiantes.sh
+++ instalar-condor-estudiantes.sh
@@ -19,31 +19,57 @@ rm -rfv /tmp/rpms
 
 # SELINUX
 sudo sed -i.bak 's/^SELINUX=.*/SELINUX=permissive/' /etc/selinux/config
+sudo setenforce permissive
 
 # Repositorio EPEL
 sudo yum install -y epel-release
 sudo yum update -y
 
-# MariaDB
+# MARIADB
 sudo yum install -y mariadb-server mariadb
 sudo systemctl enable mariadb
 sudo systemctl start mariadb
-#sudo mysql_secure_installation # TODO: este comando es asistido se debe modificar
+
+# inicio mysql_secure_installation
+# esto funciona, pero quizá debería hacerse en la instancia real para que no todas terminen con el mismo password de root o quizá esto no es un problema
+newpass="$(uuidgen)"
+sudo tee /var/lib/mysql_secure_installation_answers > /dev/null << EOF
+
+Y
+$newpass
+$newpass
+Y
+Y
+Y
+EOF
+sudo chmod 400 /var/lib/mysql_secure_installation_answers
+sudo cat /var/lib/mysql_secure_installation_answers | sudo mysql_secure_installation
+# fin mysql_secure_installation
+
+sudo systemctl stop mariadb # después de configurar la base la podemos parar
 
 # APACHE
-sudo yum -y install httpd
-sudo systemctl enable httpd #Al inicio arranca apache
-sudo apachectl start
+sudo yum install -y httpd
+sudo systemctl enable httpd
+# sudo apachectl start # mientras se crea la ami no es necesario iniciar cosas
 
 # PHP
-sudo yum -y install php
+sudo yum install -y php
 
 # PHPMYADMIN
-sudo yum -y install phpmyadmin 
-# Se comentan las líneas Require ip 127.0.0.1 y Require ip ::1
-# Se agrega la línea Require all granted
-sudo sed -i -- 's/Require ip 127.0.0.1/#Require ip 127.0.0.1/g' /etc/httpd/conf.d/phpMyAdmin.conf
-sudo sed -i -- 's/Require ip ::1/#Require ip ::1/g' /etc/httpd/conf.d/phpMyAdmin.conf
-sudo sed -i -- '/Require ip ::1/a\ \ \ \ \ \ \ Require all granted' /etc/httpd/conf.d/phpMyAdmin.conf
+sudo yum install -y phpmyadmin
+# Se comentan las líneas "Require ip 127.0.0.1" y "Require ip ::1"
+# Se agrega la línea "Require all granted"
+sudo sed -i.packer-bak \
+  -e 's/Require ip 127.0.0.1/#Require ip 127.0.0.1/g' \
+  -e 's/Require ip ::1/#Require ip ::1/g' \
+  -e '/Require ip ::1/a\ \ \ \ \ \ \ Require all granted' \
+  /etc/httpd/conf.d/phpMyAdmin.conf
 
-sudo apachectl restart
+if diff /etc/httpd/conf.d/phpMyAdmin.conf.packer-bak /etc/httpd/conf.d/phpMyAdmin.conf
+then
+  echo sin cambios en phpMyAdmin.conf
+fi
+
+# sudo apachectl restart # mientras se crea la ami no es necesario iniciar cosas
+sudo apachectl -t
