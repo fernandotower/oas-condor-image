3484ad8f58829851bd48700b3869faa8ea2ad204
andresvia <andres.via@gmail.com>
2016-06-26 23:24:24 -0500

    no se requiere linea de comandos de mysql

--- scripts/instalar-paquetes.sh
+++ scripts/instalar-paquetes.sh
@@ -3,7 +3,7 @@
 # rationale: figlet: mensajes claramente visibles durante la instalación
 # rationale: git: descargar código desde repositorio central
 # rationale: ntp: mantener el servidor con la hora precisa
-# rationale: httpd, mariadb, php, php-mysql, php-pgsql: la aplicación es LAMP
+# rationale: httpd, php, php-mysql, php-pgsql: la aplicación es LAMP
 # rationale: gcc, make, php-pear, php-devel: por licencias de Oracle los controladores para PHP (OCI8) solo se distribuyen a manera de código fuente, deben compilarse para poderse usar, estos paquetes se necesitan para poder compilar OCI8
 # rationale: awscli: interactuar con AWS programáticamente
 # rationale: jq: procesar respuestas en JSON de diversas APIs
@@ -14,4 +14,4 @@ figlet -f banner paquetes
 
 set -eu
 
-sudo yum install -y git mariadb ntp httpd php php-mysql gcc make php-pear php-devel awscli jq php-pgsql
+sudo yum install -y git ntp httpd php php-mysql gcc make php-pear php-devel awscli jq php-pgsql
