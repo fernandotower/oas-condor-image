c9f1373c1f93b418368ce15d36254b066e3829e6
andresvia <andres.via@gmail.com>
2016-06-30 03:58:10 -0500

    subiendo paquetes

--- paquetes.sh
+++ paquetes.sh
@@ -2,6 +2,15 @@
 
 set -eu
 
-aws 
+base_s3="s3://${STACK_CloudFormerRepositorioRPM}/rpms"
 
-aws s3 cp "*.rpm" "s3://${STACK_CloudFormerRepositorioRPM}/rpms/"
+for rpm in *.rpm
+do
+  if ! aws s3 ls "${base_s3}/${rpm}"
+  then
+    echo copiando $rpm al bucket $base_s3
+    aws s3 cp "${rpm}" "${base_s3}"
+  else
+    echo $rpm ya esta en el bucket $base_s3
+  fi
+done
